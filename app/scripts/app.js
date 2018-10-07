5265d80f3825168684b41ca66cb088eb34eb4bc4
Beatrize <heybeatrize@gmail.com>
2018-10-07 13:03:54 -0700

    edited thumb-start app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -64,6 +64,18 @@ Instructions:
 
     Your code goes here!
      */
-    // getJSON('../data/earth-like-results.json')
+     getJSON('../data/earth-like-results.json')
+     .then(function(response) {
+       addSearchHeader(response.query);
+       return getJSON(response.results[0]);
+     })
+     .catch(function() {
+       throw Error('Search Request Error');
+     })
+     .then(createPlanetThumb)
+     .catch(function(e) {
+       addSearchHeader('unknown');
+       console.log(e);
+     })
   });
 })(document);
