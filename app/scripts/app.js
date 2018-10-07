5be4ac22e6e71bd7fbe989e6da9eae9fd2a2dcfc
Beatrize <heybeatrize@gmail.com>
2018-10-07 13:45:16 -0700

    edited map-start app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -59,6 +59,11 @@ Instructions:
     Your code goes here! Uncomment the next line when you're ready to start!
      */
 
-    // getJSON('../data/earth-like-results.json')
+     getJSON('../data/earth-like-results.json')
+     .then(function(response) {
+       response.results.map(function(url) {
+        getJSON(url).then(createPlanetThumb);
+      });
+    });
   });
 })(document);
